9cef7d2d47f47b94026e18ea4050143b7b041a5a
AtanasRaev <atanaspraev@gmail.com>
2025-03-25 00:05:22 +0200

    Created AdminSettings entity

---
+++ src/main/java/com/storemanagement/database/model/AdminSettings.java
@@ -0,0 +1,18 @@
+package com.storemanagement.database.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "admin_settings")
+public class AdminSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String password;
+}
