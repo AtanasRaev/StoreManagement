b60d06211836482266f2e968ef2999a9736ab234
AtanasRaev <atanaspraev@gmail.com>
2025-03-25 01:06:59 +0200

    Add PurchaseRepository repository

---
+++ src/main/java/com/storemanagement/database/repository/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package com.storemanagement.database.repository;
+
+import com.storemanagement.database.model.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+}
