fe7cee93b7e3b6285025c74559f36f7d55d8b080
AtanasRaev <atanaspraev@gmail.com>
2025-03-25 01:06:47 +0200

    Add ProductRepository repository

---
+++ src/main/java/com/storemanagement/database/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.storemanagement.database.repository;
+
+import com.storemanagement.database.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
