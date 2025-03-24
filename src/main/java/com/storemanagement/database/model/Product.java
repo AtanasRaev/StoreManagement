d3e69d779bcfc4d26a0d6ae1b566e9b293f4639d
AtanasRaev <atanaspraev@gmail.com>
2025-03-24 23:58:11 +0200

    Created Product entity

---
+++ src/main/java/com/storemanagement/database/model/Product.java
@@ -0,0 +1,38 @@
+package com.storemanagement.database.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal quantity;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal  price;
+
+    @Column(nullable = false)
+    private String unit;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated", nullable = false)
+    private LocalDateTime lastUpdated;
+
+    @Column(name = "is_synced", nullable = false)
+    private boolean isSynced = false;
+}
