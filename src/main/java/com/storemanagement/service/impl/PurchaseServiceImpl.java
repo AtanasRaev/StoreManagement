1edd5eacaf1be5a93313e443a142421db4dbab36
AtanasRaev <atanaspraev@gmail.com>
2025-03-28 19:24:17 +0200

    Add PurchaseServiceImpl class

---
+++ src/main/java/com/storemanagement/service/impl/PurchaseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.storemanagement.service.impl;
+
+import com.storemanagement.service.PurchaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseServiceImpl implements PurchaseService {
+}
