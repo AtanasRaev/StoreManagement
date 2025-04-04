a2961dfc9a742325bbabd0d42cf067bb3ad9f9c9
AtanasRaev <atanaspraev@gmail.com>
2025-04-04 22:47:35 +0300

    Add create and getAll methods

--- src/main/java/com/storemanagement/service/PurchaseService.java
+++ src/main/java/com/storemanagement/service/PurchaseService.java
@@ -1,4 +1,12 @@
 package com.storemanagement.service;
 
+import com.storemanagement.database.dto.PurchaseCreateDTO;
+import com.storemanagement.database.dto.PurchasePageDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PurchaseService {
+    boolean create(PurchaseCreateDTO purchaseCreateDTO);
+
+    Page<PurchasePageDTO> getAll(Pageable pageable);
 }
