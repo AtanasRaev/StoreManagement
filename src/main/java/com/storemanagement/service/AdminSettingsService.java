858aaaa2991d9125f5a9421bc0e382757bc79b7d
AtanasRaev <atanaspraev@gmail.com>
2025-04-04 22:35:39 +0300

    Removed methods has-password and register

--- src/main/java/com/storemanagement/service/AdminSettingsService.java
+++ src/main/java/com/storemanagement/service/AdminSettingsService.java
@@ -3,9 +3,5 @@ package com.storemanagement.service;
 import com.storemanagement.database.dto.PasswordDTO;
 
 public interface AdminSettingsService {
-    boolean hasPasswordSet();
-
-    void setPassword(PasswordDTO dto);
-
     boolean login(PasswordDTO dto);
 }
