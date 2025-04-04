e3b449e17388e38be3357461ef8397fecb8cdfd7
AtanasRaev <atanaspraev@gmail.com>
2025-04-04 22:48:37 +0300

    Add PurchasePageDTO

---
+++ src/main/java/com/storemanagement/database/dto/PurchasePageDTO.java
@@ -0,0 +1,22 @@
+package com.storemanagement.database.dto;
+
+import com.storemanagement.database.model.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PurchasePageDTO {
+    private Long id;
+
+    private Product product;
+
+    private BigDecimal quantity;
+
+    private BigDecimal  priceAtTime;
+
+    private LocalDateTime createdAt;
+}
