ce723c66d382710d7a6b41639fe897b00162d65a
AtanasRaev <atanaspraev@gmail.com>
2025-04-14 21:03:33 +0300

    Add quantitySold, image and unit fields and format date

--- src/main/java/com/storemanagement/database/dto/ReportCheckDTO.java
+++ src/main/java/com/storemanagement/database/dto/ReportCheckDTO.java
@@ -1,5 +1,6 @@
 package com.storemanagement.database.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,26 +10,34 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ReportCheckDTO {
-    private Long id;
-
+    @JsonFormat(pattern = "dd-MM-yyyy", timezone = "Europe/Sofia")
     private LocalDateTime loadedAt;
 
     private BigDecimal quantityLoaded;
 
     private BigDecimal quantityNow;
 
+    private BigDecimal quantitySold;
+
     private BigDecimal scrap;
 
     private String product;
 
+    private String image;
+
+    private String unit;
+
     public ReportCheckDTO(LocalDateTime loadedAt,
                           BigDecimal quantityLoaded,
                           BigDecimal quantityNow,
-                          String product) {
+                          String product,
+                          String unit,
+                          String image) {
         this.loadedAt = loadedAt;
         this.quantityLoaded = quantityLoaded;
         this.quantityNow = quantityNow;
         this.product = product;
+        this.unit = unit;
+        this.image = image;
     }
-
 }
