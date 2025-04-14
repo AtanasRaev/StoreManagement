89cdaab470f8886fd03442f2d859bbb3dc1b974f
AtanasRaev <atanaspraev@gmail.com>
2025-04-14 16:54:52 +0300

    Refactor findAllBySelectedIs method with custom query

--- src/main/java/com/storemanagement/database/repository/ProductRepository.java
+++ src/main/java/com/storemanagement/database/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.storemanagement.database.repository;
 
 import com.storemanagement.database.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
 
-    List<Product> findAllBySelectedIs(boolean selected);
+    @Query("SELECT p FROM Product p WHERE p.isSelected = :selected")
+    List<Product> findAllBySelectedIs(@Param("selected") boolean selected);
 }
