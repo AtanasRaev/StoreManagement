d0d82e24d05913bdce78f8c68b5a2a270f80d5d0
AtanasRaev <atanaspraev@gmail.com>
2025-04-14 16:53:58 +0300

    Add Report entity

---
+++ src/main/java/com/storemanagement/database/model/Report.java
@@ -0,0 +1,41 @@
+package com.storemanagement.database.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reports")
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "integer primary key autoincrement")
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Product product;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "is_synced", nullable = false)
+    private boolean isSynced;
+
+    public Report(Product product, BigDecimal quantity, LocalDateTime createdAt, boolean isSynced) {
+        this.product = product;
+        this.quantity = quantity;
+        this.createdAt = createdAt;
+        this.isSynced = isSynced;
+    }
+
+    public Report() {
+    }
+}
