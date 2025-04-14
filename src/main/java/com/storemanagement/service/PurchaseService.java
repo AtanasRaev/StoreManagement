dfdbcb6f73dec189bdf053fbb1c54bcea703ba0c
AtanasRaev <atanaspraev@gmail.com>
2025-04-14 18:18:10 +0300

    Add getPurchaseQuantityInPeriod method

--- src/main/java/com/storemanagement/service/PurchaseService.java
+++ src/main/java/com/storemanagement/service/PurchaseService.java
@@ -2,11 +2,17 @@ package com.storemanagement.service;
 
 import com.storemanagement.database.dto.PurchaseCreateDTO;
 import com.storemanagement.database.dto.PurchasePageDTO;
+import com.storemanagement.database.dto.PurchaseQuantityDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PurchaseService {
     boolean create(PurchaseCreateDTO purchaseCreateDTO);
 
     Page<PurchasePageDTO> getAll(Pageable pageable);
+
+    List<PurchaseQuantityDTO> getPurchaseQuantityInPeriod(LocalDateTime startDate, LocalDateTime endDate);
 }
