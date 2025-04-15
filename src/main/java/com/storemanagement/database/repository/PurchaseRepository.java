011a99b02b0488d07f969d81cf48673f35c4c5cb
AtanasRaev <atanaspraev@gmail.com>
2025-04-15 21:56:30 +0300

    Add new methods

--- src/main/java/com/storemanagement/database/repository/PurchaseRepository.java
+++ src/main/java/com/storemanagement/database/repository/PurchaseRepository.java
@@ -1,6 +1,6 @@
 package com.storemanagement.database.repository;
 
-import com.storemanagement.database.dto.PurchasePageDTO;
+import com.storemanagement.database.dto.PurchaseCloudDTO;
 import com.storemanagement.database.dto.PurchaseQuantityDTO;
 import com.storemanagement.database.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,11 +13,15 @@ import java.util.List;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-    @Query("SELECT new com.storemanagement.database.dto.PurchaseQuantityDTO(p.product.name, SUM(p.quantity)) " +
+    @Query("SELECT new com.storemanagement.database.dto.PurchaseQuantityDTO(p.product.name, SUM(p.quantity), SUM(p.price)) " +
             "FROM Purchase p " +
             "WHERE p.createdAt BETWEEN :startDate AND :endDate " +
             "GROUP BY p.product.name")
     List<PurchaseQuantityDTO> findPurchaseInPeriod(@Param("startDate") LocalDateTime startDate,
                                                    @Param("endDate") LocalDateTime endDate);
 
+    @Query("SELECT p FROM Purchase p WHERE p.isSynced = false")
+    List<PurchaseCloudDTO> findAllNotSynced();
+
+    List<Purchase> findAllByIdIn(List<Long> appIds);
 }
